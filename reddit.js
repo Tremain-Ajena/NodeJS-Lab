d61bde959316ad28dc80aa2ef4e0a2cd3350cdff
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-03-06 14:10:26 -0600

    finished lab

--- reddit.js
+++ reddit.js
@@ -1,26 +1,55 @@
-function parse(){
-    return new Promise(function(resolve, reject){
-        request('https://reddit.com/r/popular.json', function (error, response, body) {
-            // in addition to parsing the value, deal with possible errors
-            if (error) return reject(error);
-            try {
-                // JSON.parse() can throw an exception if not valid JSON
-                resolve(JSON.parse(body).data.available_balance);
-            } catch(e) {
-                reject(e);
-            }
-        });
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+let dataPath = path.join(__dirname, '../popular-articles.json');
+
+request('https://reddit.com/r/popular.json', function (error, res, body) {
+    // in addition to parsing the value, deal with possible errors
+    if (error) {
+        console.log(error);
+    };
+    JSON.parse(body).data.children.forEach(item => {
+        console.log(item.data.title, item.data.url, item.data.author);
     });
-}
-
-
-
-var rp = require('request-promise');
-
-rp('https://reddit.com/r/popular.json'
-    .then(function (response) {
-        // resolved
-    })
-    .catch(function (err) {
-        // rejected
+    fs.writeFile(dataPath, res.body, error => {
+        if (error) {
+            console.log(error);
+        }
     });
+});
+
+// const fetch = require('node-fetch');
+
+// fetch('https://reddit.com/r/popular.json')
+//     .then(data => popular - articles.json())
+//     .then(data => {
+//         JSON.parse(body).data.children.forEach(item => {
+//             // console.log(item.data.title);
+//             console.log(
+//                 item.data.title,
+//                 item.data.url,
+//                 item.data.author
+//             )
+//         });
+//     })
+//     .then(info => {
+//         fs.writeFile(dataPath, res.body, error => {
+//             if (error) {
+//                 console.log(error);
+//             }
+//         })
+//             .catch(error => {
+//                 error = 'Could not find information'
+//             })
+
+
+
+// var rp = require('request-promise');
+
+// rp('https://reddit.com/r/popular.json'
+//     .then(function (response) {
+//         // resolved
+//     })
+//     .catch(function (err) {
+//         // rejected
+//     });
